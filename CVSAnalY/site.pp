c6cc7b6d56b805c3e901f199274bb1ed191983fd
david <dpose@bitergia.com>
2015-03-09 16:18:40 +0100

    Deleted old files

--- CVSAnalY/site.pp
+++
@@ -1,5 +0,0 @@
-<%# Here you import your different nodes.pp and you can define global variables. %>
-import 'nodes.pp'
-
-$puppetmaster = 'test1.domain.test1' <%# test1.domain.test1 must be changed for your puppetmaster's name. %>
-$mysql_password = 'root' <%# root must be changed for your current root password. %>
