329fa4ea59308b2361a712122dfbd1f25d03e941
david <dpose@bitergia.com>
2015-03-16 16:45:38 +0100

    new version with some bugs fixed

--- CVSAnalY/Recipe/cvsanaly.pp
+++ CVSAnalY/Recipe/cvsanaly.pp
@@ -1,8 +1,8 @@
-<%# This class installs CVSAnalY tool and all necessary packages to get it. %>
+# This class installs CVSAnalY tool and all necessary packages to get it.
 
 class cvsanaly {
 
-<%# 1. PATH and .my.cnf file (necessary to connect to mysql ddbb without pass). %>
+# 1. PATH and .my.cnf file (necessary to connect to mysql ddbb without pass).
 	$path = ["/usr/local/sbin","/usr/local/bin","/usr/sbin","/usr/bin","/sbin","/bin"]
 
 	file { "/root/.my.cnf":
@@ -32,46 +32,46 @@ class cvsanaly {
                 path ==> $path
 	} 
 
-<%# 2. First at all, apt-get update. %>
+# 2. First at all, apt-get update.
 	exec { "apt-get update":
 		user => root,
 		path => $path
 	}
 
-<%# 3. Installation and configuration of mysql-server. %>
+# 3. Installation and configuration of mysql-server.
 	class { '::mysql::server':
 		old_root_password => $mysql_password,
 		root_password => $mysql_password,
 		override_options => { root_password => $mysql_password }
 	}
 
-<%# 4. Installation of mysql-client. %>
+# 4. Installation of mysql-client.
 	class { '::mysql::client': }
 
-<%# 5. A new mysql-ddbb and mysql-user are created. %>
+# 5. A new mysql-ddbb and mysql-user are created.
 	mysql::db { 'testdb':
 		user => 'usertest',
 		password => 'usertest',
 		grant => 'ALL'
 	}
 
-<%# 6. Installation of git package. %>
+# 6. Installation of git package.
 	package { "git":
 		ensure => installed,
 		require => Exec["apt-get update"]
 	}
 
-<%# 7. A new directory to git clone CVSAnaly repo is created. %>
+# 7. A new directory to git clone CVSAnaly repo is created.
 	file { "/home/git/CVSAnalY":
 		ensure => "directory"
 	}
 	
-<%# 8. A new directory to git clone RepositoryHandler repo is created. %>
+# 8. A new directory to git clone RepositoryHandler repo is created.
 	file { "/home/git/RepositoryHandler":
 		ensure => "directory"
 	}
 
-<%# 9. git clone CVSAnaly repo. %>
+# 9. git clone CVSAnaly repo.
 	vcsrepo { "/home/git/CVSAnalY":
 		ensure => present,
 		provider => git,
@@ -79,7 +79,7 @@ class cvsanaly {
 		revision => 'master'
 	}
 	
-<%# 10. git clone RepositoryHandler repo. %>
+# 10. git clone RepositoryHandler repo.
 	vcsrepo { "/home/git/RepositoryHandler":
 		ensure => present,
 		provider => git,
@@ -87,20 +87,20 @@ class cvsanaly {
 		revision => 'master'
 	}
 
-<%# 11. Installation of repositoryHandler (setup.py). %>
+# 11. Installation of repositoryHandler (setup.py).
 	exec { "install repositoryhandler":
 		cwd => "/home/git/RepositoryHandler",
 		command => "python setup.py install",
 		path => $path
 	}
 
-<%# 12. Installation of python-setup-tools (needed for cvsanaly). %>
+# 12. Installation of python-setup-tools (needed for cvsanaly).
 	package { "python-setuptools":
 		ensure => installed,
 		require => Exec["apt-get update"]
 	}
 
-<%# 13. Installation of CVSAnalY (setup.py). %>
+# 13. Installation of CVSAnalY (setup.py).
 	exec { "install cvsanaly":
 		cwd => "/home/git/CVSAnalY",
 		command => "python setup.py install",
@@ -108,13 +108,13 @@ class cvsanaly {
 		require => Package["python-setuptools"]
 	}
 
-<%# 14. Installation of python-mysqldb (needed for cvsanaly). %>
+# 14. Installation of python-mysqldb (needed for cvsanaly).
 	package { "python-mysqldb":
 		ensure => installed,
 		require => Exec["apt-get update"]
 	}
 
-<%# 15. Configuration of mysql-password-root. %>
+# 15. Configuration of mysql-password-root.
 	exec { "mysql-password-root":
 		subscribe => [ Package["mysql-client"], Package["mysql-server"] ],
 		refreshonly => true,
--- CVSAnalY/Recipe/site.pp
+++ CVSAnalY/Recipe/site.pp
@@ -1,5 +1,5 @@
-<%# Here you import your different nodes.pp and you can define global variables. %>
+# Here you import your different nodes.pp and you can define global variables.
 import 'nodes.pp'
 
-$puppetmaster = 'test1.domain.test1' <%# test1.domain.test1 must be changed for your puppetmaster's name. %>
-$mysql_password = 'root' <%# root must be changed for your current root password. %>
+$puppetmaster = 'test1.domain.test1' # test1.domain.test1 must be changed for your puppetmaster's name.
+$mysql_password = 'root' # root must be changed for your current root password.
