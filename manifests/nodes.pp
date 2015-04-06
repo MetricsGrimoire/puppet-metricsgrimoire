72195b72146c3402348db721532ca01184ef4727
david <dpose@bitergia.com>
2015-04-06 17:32:50 +0200

    First version

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,4 +1,4 @@
 # Here is where classes are included for each host.
 	node 'test2.domain.test2' { # test2.domain.test2 must be replaced with your node's name.
-		include cvsanaly
+		include metricsgrimoire
 	}
